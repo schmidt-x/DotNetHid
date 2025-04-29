c02d2fc03f2bda1efb556e68b0374501994647a5
Stefano <schmidtsigma@gmail.com>
2025-04-29 23:00:17 +0300

    extend HidDeviceInfo

--- HidNet/HidDeviceInfo.cs
+++ HidNet/HidDeviceInfo.cs
@@ -2,15 +2,91 @@
 
 namespace HidNet;
 
-public record HidDeviceInfo(
-	UInt16 VendorId,
-	UInt16 ProductId,
-	UInt16 UsagePage,
-	UInt16 UsageId,
+/// <summary>
+/// Device description.
+/// </summary>
+public class HidDeviceInfo
+{
+	/// <summary>
+	/// HID device's vendor ID.
+	/// </summary>
+	public UInt16 VendorId { get; }
 	
-	string Path,
-	string ManufacturerString,
-	string ProductString,
+	/// <summary>
+	/// HID device's product ID.
+	/// </summary>
+	public UInt16 ProductId { get; }
 	
-	UInt16 InputReportByteLength,
-	UInt16 OutputReportByteLength);
+	/// <summary>
+	/// Manufacturer's revision number for a HIDClass device.
+	/// </summary>
+	public UInt16 VersionNumber { get; }
+	
+	/// <summary>
+	/// Top-level collection's usage page.
+	/// </summary>
+	public UInt16 UsagePage { get; }
+	
+	/// <summary>
+	/// Top-level collection's usage ID.
+	/// </summary>
+	public UInt16 UsageId { get; }
+	
+	/// <summary>
+	/// Specifies the maximum size, in bytes, of all the input reports. Includes the report ID,
+	/// which is prepended to the report data. If report ID is not used, the ID value is zero.
+	/// </summary>
+	public UInt16 InputReportByteLength { get; }
+	
+	/// <summary>
+	/// Specifies the maximum size, in bytes, of all the output reports. Includes the report ID,
+	/// which is prepended to the report data. If report ID is not used, the ID value is zero.
+	/// </summary>
+	public UInt16 OutputReportByteLength { get; }
+	
+	/// <summary>
+	/// Device interface path.
+	/// </summary>
+	public string DevicePath { get; }
+	
+	/// <summary>
+	/// Serial number string.
+	/// </summary>
+	public string SerialNumber { get; }
+	
+	/// <summary>
+	/// Manufacturer name.
+	/// </summary>
+	public string ManufacturerString { get; }
+	
+	/// <summary>
+	/// Product name.
+	/// </summary>
+	public string ProductString { get; }
+	
+	internal HidDeviceInfo(
+		UInt16 vendorId,
+		UInt16 productId,
+		UInt16 versionNumber,
+		UInt16 usagePage,
+		UInt16 usageId,
+		UInt16 inputReportByteLength,
+		UInt16 outputReportByteLength,
+		string devicePath,
+		string serialNumber,
+		string manufacturerString,
+		string productString)
+	{
+		VendorId = vendorId;
+		ProductId = productId;
+		VersionNumber = versionNumber;
+		UsagePage = usagePage;
+		UsageId = usageId;
+		InputReportByteLength = inputReportByteLength;
+		OutputReportByteLength = outputReportByteLength;
+		DevicePath = devicePath;
+		SerialNumber = serialNumber;
+		ManufacturerString = manufacturerString;
+		ProductString = productString;
+	}
+}
